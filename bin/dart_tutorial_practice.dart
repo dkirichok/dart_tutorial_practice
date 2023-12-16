86c4904e159e50816ca273b8505f085aeceacf12
Dmytro Kirichok <d.kiriczek@gmail.com>
2023-12-16 05:20:35 +0200

    Initial commit

---
+++ bin/dart_tutorial_practice.dart
@@ -0,0 +1,5 @@
+import 'package:dart_tutorial_practice/dart_tutorial_practice.dart' as dart_tutorial_practice;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_tutorial_practice.calculate()}!');
+}
---
+++ test/dart_tutorial_practice_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_tutorial_practice/dart_tutorial_practice.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
