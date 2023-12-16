01f14cb779a4b8faa63eede791cace3138ef3080
Dmytro Kirichok <d.kiriczek@gmail.com>
2023-12-16 16:51:11 +0200

    Added solutions to functions topic

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,7 +1,29 @@
-// 1. Write a function in Dart to print your own name
-// 2. Write a function in Dart to print even numbers between intervals
-// 3. Write a function in Dart that generates random password.
-// 4. Write a function in Dart that find the area of a circle
-// 5. Write a function in a dart that implements the Pythagorean theorem
-// 6. Write a function in Dart to reverse a String
-// 7. Write a function in Dart to calculate power of a certain number. For e.g 5^3=125
+import 'dart:math';
+
+// 1. Write a function in Dart to print even numbers between intervals
+List<int> evensBetweenInterval(int start, int end) {
+  return List.generate((end - start + 1), (i) => i + start)
+      .where((e) => e.isEven)
+      .toList();
+}
+
+// 2. Write a function in Dart that generates random password.
+String randomPassword([int length = 8]) {
+  return List.generate(length, (_) => Random().nextBool()
+    ? Random().nextInt(9)
+    : String.fromCharCode(97 + Random().nextInt(122 - 97)))
+    .map((e) => e is String && Random().nextBool() ? e.toUpperCase() : e)
+    .join('');
+}
+
+// 3. Write a function in Dart that find the area of a circle and round result
+// to 2 decimals
+double areaOfCircle(int radius) {
+  return double.parse((pi * pow(radius, 2)).toStringAsFixed(2));
+}
+
+// 4. Write a function in Dart to reverse a String
+String reverseString(String str) => str.split('').reversed.join('');
+
+// 5. Write a function in Dart to calculate power of a certain number (5^3=125)
+int powerOfNumber(int number, int power) => pow(number, power).toInt();
--- test/functions_test.dart
+++ test/functions_test.dart
@@ -1,8 +1,25 @@
 import 'package:test/test.dart';
-import 'package:dart_tutorial_practice/functions.dart';
+import 'package:dart_tutorial_practice/functions.dart' as functions;
 
 void main() {
-  test('Test name', () {
-    
+  test('Even numbers between interval', () {
+    expect(functions.evensBetweenInterval(2, 10), [2, 4, 6, 8, 10]);
+  });
+
+  test('Random password', () {
+    expect(functions.randomPassword().length, 8);
+  });
+
+  test('Area of circle', () {
+    expect(functions.areaOfCircle(10), 314.16);
+  });
+
+  test('Reverse string', () {
+    expect(functions.reverseString('Some'), 'emoS');
+    expect(functions.reverseString(''), '');
+  });
+
+  test('Power of number', () {
+    expect(functions.powerOfNumber(5, 2), 25);
   });
 }
