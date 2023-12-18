82d5bae7d90999a540ab470d7834d77ecae652df
Dmytro Kirichok <d.kiriczek@gmail.com>
2023-12-18 22:40:26 +0200

    Added solutions to collections

--- lib/collections.dart
+++ lib/collections.dart
@@ -1,6 +1,29 @@
-// 1. Create a program thats reads list of expenses amount using user input and print total.
-// 2. Create an empty list of type string called days. Use the add method to add names of 7 days and print all days.
-// 3. Add your 7 names to the list. Use where to find a name that starts with alphabet a.
-// 4. Create a map with name, address, age, country keys and store values to it. Update country name to other country and print all keys and values.
-// 5. Create a map with name, phone keys and store some values to it. Use where to find all keys that have length 4.
-// 6. Create a simple to-do application that allows user to add, remove, and view their task.
+void main(List<String> args) {
+  // 1. Create an empty list of type string called days. Use the add method
+  // to add names of 7 days and print all days.
+  var listOfDays = <String>[];
+  listOfDays.addAll([
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ]);
+  print(listOfDays.join('\n'));
+
+  // 2. Add your 7 names to the list. Use where to find a name that starts
+  // with alphabet a.
+  var listOfNames = ['Mason', 'Ariel', 'Macauley', 'Justine', 'Adelynn'];
+  var namesStartsWithA = listOfNames.where((name) => name[0] == 'A').toList();
+  print(namesStartsWithA);
+
+  // 3. Create a map with name, age, country keys and store values to it.
+  // Update country name to other country and print all keys and values.
+  var profile = {'name': 'John', 'age': 25, 'country': 'USA'};
+  profile['country'] = 'Canada';
+  for (var key in profile.keys) {
+    print('$key: ${profile[key]}');
+  }
+}
--- test/collection_tests.dart
+++
@@ -1,8 +0,0 @@
-import 'package:test/test.dart';
-import 'package:dart_tutorial_practice/collections.dart';
-
-void main() {
-  test('Test name', () {
-    
-  });
-}
